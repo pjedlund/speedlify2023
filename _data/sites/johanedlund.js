d4b6d7438de2f77ad9efab994679da868fc5af54
Johan Edlund <me@johanedlund.se>
2023-04-16 08:34:36 +0000

    initilal changes

---
+++ _data/sites/johanedlund.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "johanedlund.se",
+	description: "Personal website",
+	options: {
+		frequency: 60 * 1, // 1 hour
+		freshChrome: "site"
+	},
+	urls: [
+		"https://www.johanedlund.se/",
+		"https://www.johanedlund.se/posts/this-is-the-first-example-post/",
+		"https://www.johanedlund.se/cv/"
+	]
+};
