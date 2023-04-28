e32c6ad525f2725ef943f7aef73c96d8176b97af
Johan Edlund <me@johanedlund.se>
2023-04-28 18:05:15 +0000

    11tybasetemplate site added

---
+++ _data/sites/11tybasetemplate.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "11tybasetemplate",
+	description: "Template",
+	options: {
+		frequency: 60 * 2, // 2 hour
+		freshChrome: "site"
+	},
+	urls: [
+		"https://11tybasetemplate.netlify.app",
+		"https://11tybasetemplate.netlify.app/posts/illustration-of-a-bear/",
+		"https://11tybasetemplate.netlify.app/posts/illustration-of-a-seal/"
+	]
+};
