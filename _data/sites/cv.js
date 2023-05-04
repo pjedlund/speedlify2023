338687cd34e88b4bafb222e4f351a754a1554ef4
Johan Edlund <me@johanedlund.se>
2023-05-04 19:25:07 +0000

    cv.johanedlund.se added

---
+++ _data/sites/cv.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "cv.johanedlund.se",
+	description: "CV",
+	options: {
+		frequency: 60 * 2, // 2 hour
+		freshChrome: "site"
+	},
+	urls: [
+		"https://www.johanedlund.se/"
+	]
+};
