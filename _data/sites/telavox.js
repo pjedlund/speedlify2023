5d0412a4166d92813dfc555f2faf22a0574295f8
Johan Edlund <me@johanedlund.se>
2023-06-01 16:05:49 +0200

    added telavox.com

---
+++ _data/sites/telavox.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "telavox.com",
+	description: "Commercial website",
+	options: {
+		frequency: 60 * 2, // 2 hour
+		freshChrome: "site"
+	},
+	urls: [
+		"https://www.telavox.com/",
+		"https://www.telavox.com/integrations/",
+		"https://www.telavox.com/about-telavox/"
+	]
+};
