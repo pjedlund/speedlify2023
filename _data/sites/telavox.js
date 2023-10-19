a527c468890d4a422af12e4dfe43cef27814dbfc
Johan Edlund <me@johanedlund.se>
2023-10-19 18:28:44 +0200

    only one telavox url + twice a day

--- _data/sites/telavox.js
+++ _data/sites/telavox.js
@@ -1,13 +1,9 @@
 module.exports = {
-	name: "telavox.com",
-	description: "telavox.com",
-	options: {
-		frequency: 60 * 2, // 2 hour
-		freshChrome: "site"
-	},
-	urls: [
-		"https://www.telavox.com/",
-		"https://www.telavox.com/integrations/",
-		"https://www.telavox.com/about-telavox/"
-	]
+          name: 'telavox.com',
+          description: 'telavox.com',
+          options: {
+                    frequency: 60 * 12, // 12 hour
+                    freshChrome: 'site',
+          },
+          urls: ['https://www.telavox.com/'],
 };
